5328105fd603fb1ba67c6e1961baf072e1ce78b3
Evgeniy Kirianov <85870790+EvgeniyKiryanow@users.noreply.github.com>
2021-11-05 14:52:25 +0200

    create macket

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header />
+    <Main />
+  </div>
+</template>
+
+<script>
+import Header from "./components/header/HeaderComponent.vue"
+import Main from './components/main/MainComponent.vue'
+
+export default {
+  name: "App",
+  components: { Header, Main },
+};
+</script>
+
+<style lang="scss">
+body {
+    margin: 0;
+    padding: 0;
+    }
+    html {
+      font-size: 10px;
+      box-sizing: border-box;
+    }
+  #app {
+        overflow-x: hidden;
+  }
+</style>
---
+++ src/components/main/MainComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <main class="main">
+        <div class="main__wrapper">
+            <section class="main__benefit">
+                <Benefits />
+            </section>
+            <section class="main__calculate-form">
+                <From />
+            </section>
+        </div>
+    </main>
+</template>
+
+<script>
+import Benefits from './BenefitsComponent.vue'
+import From from './FormComponent.vue'
+
+export default{
+    name: "Main",
+    components: { Benefits, From },
+}
+</script>
+
+<style lang="scss" scoped>
+    .main {
+        background-image: url("../../../src/assets/main-img.jpeg");
+        background-repeat: no-repeat;
+        background-size: 45%;
+        background-position-x: 100%;
+        &__wrapper {
+            display: flex;
+            max-width: 1024px;
+            width: 100%;
+            margin: 0 auto;
+        }
+        &__benefit {
+            width: 57%;
+        }
+        &__calculate-form {
+            width: 43%;
+            position: relative;
+        }
+    }
+</style>
